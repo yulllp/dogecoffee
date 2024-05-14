8f0e9eacc28a79e2a361b6b408c879710c19c891
louisarthurw <c14210048@john.petra.ac.id>
2024-05-14 21:23:49 +0700

    login page done

--- lib/login.dart
+++ lib/login.dart
@@ -14,183 +14,215 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 80),
-              child: Container(
-                height: 225,
-                child: Image.asset(
-                  "assets/bigLogo.png",
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 50, left: 30, right: 30),
-            child: TextFormField(
-              style: TextStyle(
-                  color: Color.fromRGBO(227, 227, 227, 1.0), fontSize: 18),
-              cursorColor: Color.fromRGBO(227, 227, 227, 1.0),
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.email_outlined,
-                    color: Color.fromRGBO(227, 227, 227, 0.75)),
-                filled: true,
-                fillColor: Color.fromRGBO(39, 39, 39, 1),
-                focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Color.fromRGBO(227, 227, 227, 1.0))),
-                enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Color.fromRGBO(227, 227, 227, 1.0))),
-                labelText: 'Email',
-                labelStyle: TextStyle(
-                    color: Color.fromRGBO(227, 227, 227, 0.75),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
-            child: TextFormField(
-              obscureText: true,
-              cursorColor: Color.fromRGBO(205, 115, 115, 1),
-              style: TextStyle(
-                  color: Color.fromRGBO(227, 227, 227, 1.0), fontSize: 18),
-              decoration: const InputDecoration(
-                filled: true,
-                fillColor: Color.fromRGBO(39, 39, 39, 1),
-                labelText: 'Password',
-                labelStyle: TextStyle(
-                    color: Color.fromRGBO(227, 227, 227, 0.75),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-                prefixIcon: Icon(
-                  Icons.lock_outline_sharp,
-                  color: Color.fromRGBO(227, 227, 227, 0.75),
-                ),
-                // helperText: "*Password length must be more than 4 characters",
-                // helperStyle: TextStyle(
-                //   color: Color.fromRGBO(227, 227, 227, 0.75),
-                // ),
-                enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Color.fromRGBO(227, 227, 227, 1.0))),
-                focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Color.fromRGBO(227, 227, 227, 1.0))),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 25, right: 30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
               children: [
-                Row(
-                  children: [
-                    Checkbox(
-                      visualDensity: VisualDensity(horizontal: -4.0),
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked = value!;
-                        });
-                      },
-                      value: isChecked,
-                      checkColor: Color.fromRGBO(227, 227, 227, 1.0),
-                      side:
-                          BorderSide(color: Color.fromRGBO(227, 227, 227, 1.0)),
-                      fillColor: MaterialStateProperty.resolveWith((states) {
-                        return Color.fromRGBO(39, 39, 39, 1);
-                      }),
-                    ),
-                    Text(
-                      "Remember me",
-                      style: TextStyle(
-                        color: Color.fromRGBO(227, 227, 227, 0.75),
-                        fontSize: 16,
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 80),
+                    child: Container(
+                      height: 225,
+                      child: Image.asset(
+                        "assets/bigLogo.png",
+                        fit: BoxFit.fill,
                       ),
-                      textAlign: TextAlign.start,
                     ),
-                  ],
+                  ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    // Handle forget password tap
-                  },
-                  child: Text(
-                    'Forget Password?',
+                Padding(
+                  padding: const EdgeInsets.only(top: 50, left: 30, right: 30),
+                  child: TextFormField(
+                    cursorColor: Color.fromRGBO(227, 227, 227, 1.0),
                     style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 16,
+                      color: Color.fromRGBO(227, 227, 227, 1.0),
+                      fontSize: 18,
                     ),
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromRGBO(39, 39, 39, 1),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: Color.fromRGBO(227, 227, 227, 0.75),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      prefixIcon: Icon(
+                        Icons.email_outlined,
+                        color: Color.fromRGBO(227, 227, 227, 0.75),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(227, 227, 227, 1.0),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(227, 227, 227, 1.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+                  child: TextFormField(
+                    obscureText: true,
+                    cursorColor: Color.fromRGBO(205, 115, 115, 1),
+                    style: TextStyle(
+                      color: Color.fromRGBO(227, 227, 227, 1.0),
+                      fontSize: 18,
+                    ),
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromRGBO(39, 39, 39, 1),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: Color.fromRGBO(227, 227, 227, 0.75),
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      prefixIcon: Icon(
+                        Icons.lock_outline_sharp,
+                        color: Color.fromRGBO(227, 227, 227, 0.75),
+                      ),
+                      // helperText: "*Password length must be more than 4 characters",
+                      // helperStyle: TextStyle(
+                      //   color: Color.fromRGBO(227, 227, 227, 0.75),
+                      // ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(227, 227, 227, 1.0),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(227, 227, 227, 1.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 25, right: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Checkbox(
+                            visualDensity: VisualDensity(horizontal: -4.0),
+                            onChanged: (bool? value) {
+                              setState(() {
+                                isChecked = value!;
+                              });
+                            },
+                            value: isChecked,
+                            checkColor: Color.fromRGBO(227, 227, 227, 1.0),
+                            side: BorderSide(
+                              color: Color.fromRGBO(227, 227, 227, 1.0),
+                            ),
+                            fillColor: MaterialStateProperty.resolveWith(
+                              (states) {
+                                return Color.fromRGBO(39, 39, 39, 1);
+                              },
+                            ),
+                          ),
+                          Text(
+                            "Remember me",
+                            style: TextStyle(
+                              color: Color.fromRGBO(227, 227, 227, 0.75),
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Handle forget password tap
+                        },
+                        child: Text(
+                          'Forget Password?',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 10,
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Action when login button is pressed
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromRGBO(39, 39, 39, 1),
+                        ),
+                        side: MaterialStateProperty.all<BorderSide>(
+                          BorderSide(
+                            color: Color.fromRGBO(227, 227, 227, 1.0),
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Color.fromRGBO(227, 227, 227, 0.75),
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      bottom: 25.0, left: 30.0, right: 30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                          color: Color.fromRGBO(227, 227, 227, 0.75),
+                          fontSize: 16,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Action when "Sign up" text is tapped
+                        },
+                        child: Text(
+                          "Sign up",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-          Spacer(),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Action when login button is pressed
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    Color.fromRGBO(39, 39, 39, 1),
-                  ),
-                  side: MaterialStateProperty.all<BorderSide>(
-                    BorderSide(color: Color.fromRGBO(227, 227, 227, 1.0)), // Border color for all states
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Color.fromRGBO(227, 227, 227, 0.75),
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.only(bottom: 25.0, left: 30.0, right: 30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account? ",
-                  style: TextStyle(
-                    color: Color.fromRGBO(227, 227, 227, 0.75),
-                    fontSize: 16,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    // Action when "Sign up" text is tapped
-                  },
-                  child: Text(
-                    "Sign up",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
