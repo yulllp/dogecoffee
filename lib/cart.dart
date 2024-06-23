95f81903a1e784945dbae32c0b675f6913b3840a
Ericksen77 <c14210151@john.petra.ac.id>
2024-06-23 23:17:40 +0700

    uhuyy

--- lib/cart.dart
+++ lib/cart.dart
@@ -347,7 +347,7 @@ class _CartPageState extends State<CartPage> {
                               ),
                             ],
                           )
-                        : Container(),
+                        : SizedBox.shrink(),
                   )
                 ],
               ),
